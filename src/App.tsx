02654ee1a148aa0cf8d4f65f6d801bf4edaf1494
Dang Le <haidang.levn@gmail.com>
2023-03-17 10:04:06 +0200

    Add react bootstrap nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import './App.css';
+import StoreNavBar from './components/StoreNavBar';
 
 function App() {
   return (
     <div className="App">
+      <StoreNavBar/>
       App will be here
     </div>
   );
